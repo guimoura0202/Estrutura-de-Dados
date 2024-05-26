9d831c36d4bbaf7362724e3ff5cf66272019773b
guimoura0202 <guiavilamou@gmail.com>
2024-05-25 22:27:48 -0300

    Atividade 6

---
+++ Atividade6/Main.java
@@ -0,0 +1,24 @@
+
+public class Main{
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(1000);
+    
+        // Medir tempo de execução do método recursivo
+        long inicioRecursivo = System.nanoTime();
+        int maiorRecursivo = vetor.maiorElementoRecursivo();
+        long fimRecursivo = System.nanoTime();
+        long tempoRecursivo = fimRecursivo - inicioRecursivo;
+    
+        // Medir tempo de execução do método iterativo
+        long inicioIterativo = System.nanoTime();
+        int maiorIterativo = vetor.maiorElementoIterativo();
+        long fimIterativo = System.nanoTime();
+        long tempoIterativo = fimIterativo - inicioIterativo;
+    
+        // Imprimir resultados
+        System.out.println("Maior elemento (Recursivo): " + maiorRecursivo);
+        System.out.println("Tempo de execução (Recursivo): " + tempoRecursivo + " nanosegundos");
+        System.out.println("Maior elemento (Iterativo): " + maiorIterativo);
+        System.out.println("Tempo de execução (Iterativo): " + tempoIterativo + " nanosegundos");
+    }
+}
---
+++ Atividade6/Vetor.java
@@ -0,0 +1,38 @@
+
+import java.util.Random;
+
+public class Vetor {
+    private int[] elementos;
+
+    public Vetor(int tamanho) {
+        this.elementos = new int[tamanho];
+        Random random = new Random();
+        for (int i = 0; i < tamanho; i++) {
+            this.elementos[i] = random.nextInt(10000); // Números aleatórios entre 0 e 9999
+        }
+    }
+
+    public int maiorElementoRecursivo() {
+        return maiorElementoRecursivoHelper(0, elementos[0]);
+    }
+
+    private int maiorElementoRecursivoHelper(int index, int maxAtual) {
+        if (index == elementos.length) {
+            return maxAtual;
+        }
+        if (elementos[index] > maxAtual) {
+            maxAtual = elementos[index];
+        }
+        return maiorElementoRecursivoHelper(index + 1, maxAtual);
+    }
+
+    public int maiorElementoIterativo() {
+        int maxElemento = elementos[0];
+        for (int i = 1; i < elementos.length; i++) {
+            if (elementos[i] > maxElemento) {
+                maxElemento = elementos[i];
+            }
+        }
+        return maxElemento;
+    }
+}
